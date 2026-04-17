3a3fa74dfd9c67fef99d8a5b190ffe56eea104cc
senanisoyza <senanisoyza@gmail.com>
2026-04-17 19:12:30 +0530

    Update urls.py
    removed dashboard URL

--- transactions/urls.py
+++ transactions/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
-    path('', views.dashboard, name='dashboard'), 
+urlpatterns = [ 
     path('add/', views.add_transaction, name='add_transaction'),
     path('list/', views.transaction_list, name='transaction_list'),
     path('edit/<int:pk>/', views.edit_transaction, name='edit_transaction'),

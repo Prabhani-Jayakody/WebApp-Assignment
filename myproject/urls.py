9d94431ce972e56a50b36395895f56723f785c7e
senanisoyza <senanisoyza@gmail.com>
2026-04-17 19:14:15 +0530

    Update urls.py

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.shortcuts import redirect 
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('transactions/', include('transactions.urls')),
     path('', lambda request: redirect('dashboard', permanent=False)),
+    path('', RedirectView.as_view(url='/transactions/', permanent=False)),
 ]
